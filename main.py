ec4564956dc948c5da2d529072d7bf6dbbad30e3
Deepika S R <deepikasanathanan1234@gmail.com>
2023-05-10 23:06:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import pyperclip
+import random
+import streamlit as st
+import string
+
+st.title("Password Generator using 🐍")
+st.title("")
+
+char = st.slider('Password length', 0, 25)
+
+password = ''.join(random.choices(string.ascii_letters + string.digits, k=char))
+
+st.subheader(password)
+st.title("")
+if st.button("Generate and Copy to clipboard"):
+        pyperclip.copy(password)
+        st.success('Text copied to clipboard!')
